40539b68f6cbd80a06a0a2b3a4975f2af9b6b986
Akshata Vyas <akshatavyas01@gmail.com>
2026-03-12 12:30:43 +0530

    Database message storing and fetching added.

--- main/UI.py
+++ main/UI.py
@@ -3,9 +3,13 @@ from agent import agent
 import streamlit as st
 from agent import agent
 from langchain_core.messages import HumanMessage
+import sqlite3
 
-if "messages" not in st.session_state:
-    st.session_state.messages = []
+conn=sqlite3.connect(r"D:\PRACTICE AGENT\LANGCHAIN\main\database.db")
+cursor=conn.cursor()
+
+# if "messages" not in st.session_state:
+#     st.session_state.messages = []
 
 st.title("Word Problem Solver Grade-5")
 st.subheader("Enter the word Problem and receive the solution:")
@@ -30,19 +34,35 @@ if st.button("Solve") and problem:
                     stream_text = msg.content
                     placeholder.write(stream_text)
 
+    cursor.execute("INSERT INTO messages (Human, AI) VALUES (?, ?)",(problem, stream_text))
+    conn.commit()
 
-    message = {
-        "Human_message": problem,
-        "AI_message": stream_text
-    }
 
-    st.session_state.messages.append(message)
+cursor.execute("SELECT Human, AI FROM messages ORDER BY ID DESC LIMIT 5 ")
+rows=cursor.fetchall()
 
-if st.session_state.messages:
-    for msg in st.session_state.messages:
-        st.markdown("**Question**")
-        st.write(msg["Human_message"])
+for row in rows:
+    st.markdown("**Question**")
+    st.write(row[0])
+    st.markdown("**Answer**")
+    st.write(row[1])
+    st.divider()
 
-        st.markdown("**Answer**")
-        st.write(msg["AI_message"])
-        st.divider()
+
+conn.close()
+
+    # message = {
+    #     "Human_message": problem,
+    #     "AI_message": stream_text
+    # }
+
+    # st.session_state.messages.append(message)
+
+# if st.session_state.messages:
+    # for msg in st.session_state.messages:
+    #     st.markdown("**Question**")
+    #     st.write(msg["Human_message"])
+
+    #     st.markdown("**Answer**")
+    #     st.write(msg["AI_message"])
+    #     st.divider()
